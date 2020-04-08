01c77ca910a44db66a3cbccec36ca91efa16d416
Stephan Johansen <stephan.johansen@outlook.com>
2020-04-08 10:56:48 +0200

    Initial

---
+++ WebApplication2/Controllers/QueryController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using EntityGraphQL;
+using EntityGraphQL.Schema;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Model;
+
+namespace WebApplication2.Controllers
+{
+
+    [Route("api/[controller]")]
+    public class QueryController : Controller
+    {
+        private readonly MyDbContext _dbContext;
+        private readonly SchemaProvider<MyDbContext> _schemaProvider;
+
+        public QueryController(MyDbContext dbContext, SchemaProvider<MyDbContext> schemaProvider)
+        {
+            this._dbContext = dbContext;
+            this._schemaProvider = schemaProvider;
+            _dbContext.Database.EnsureCreated();
+        }
+
+        [HttpPost]
+        public object Post([FromBody]QueryRequest query)
+        {
+            try
+            {
+                var results = _schemaProvider.ExecuteQuery(query, _dbContext, null, null);
+                // gql compile errors show up in results.Errors
+                return results;
+            }
+            catch (Exception)
+            {
+                return HttpStatusCode.InternalServerError;
+            }
+        }
+    }
+}
---
+++ WebApplication2/Model/MyModel.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Model
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Location>().HasData(
+                new Location()
+                {
+                    Id = 3,
+                    Name = "My nice location"
+                }
+            );
+        }
+
+        public DbSet<Property> Properties { get; set; }
+        public DbSet<PropertyType> PropertyTypes { get; set; }
+        public DbSet<Location> Locations { get; set; }
+    }
+
+    public class Property
+    {
+        public uint Id { get; set; }
+        public string Name { get; set; }
+        public PropertyType Type { get; set; }
+        public Location Location { get; set; }
+    }
+
+    public class PropertyType
+    {
+        public uint Id { get; set; }
+        public string Name { get; set; }
+        public decimal Premium { get; set; }
+    }
+
+    public class Location
+    {
+        public uint Id { get; set; }
+        public string Name { get; set; }
+    }
+}
